1a3feb9495299c2b60b9dba25e12554f9798d4f7
joetechem <joe@techemstudios.com>
2017-11-16 16:05:51 -0500

    simple circuit on pi

---
+++ 3_session_device/notes.py
@@ -0,0 +1,12 @@
+# Day One  
+
+    # 3D Printing
+        # typical lesson on 3D, used meetup slides
+        # intro to 123D Design, Printshop
+            # create object that solves some problem
+    # Mini lesson on ones and zeroes
+        # started sketches for LED pendants
+        # NOTE: trouble uploading code to existing pendants
+
+# Day Two
+    # circuits and pis
---
+++ 3_session_device/raspberry_pi/circuits_on_pi.md
@@ -0,0 +1,7 @@
+# Creating Basic Circuits with Raspberry Pi  
+
+![simple circuit](images/no_light.jpg)  
+
+***  
+
+![light](images/light.jpg)  
